c1c8593222c42c09a8cb3f89c04fdd817f801c6c
pnly <pnlybubbles@gmail.com>
2019-10-05 15:29:37 +0900

    wip

--- src/Main.purs
+++ src/Main.purs
@@ -5,27 +5,22 @@ import Prelude
 import Data.Char (toCharCode)
 import Data.Foldable (foldl)
 import Data.Int (toNumber)
+import Data.String (trim)
 import Fundamental (Parser, bind', char, many, oneOf, return, (<|>))
 
--- '+'
-opAdd :: Parser (Number -> Number -> Number)
-opAdd = char '+' `bind'` \_ -> return (+)
--- '-'
-opSub :: Parser (Number -> Number -> Number)
-opSub = char '-' `bind'` \_ -> return (-)
--- opA = '+' | '-'
-opAddSub :: Parser (Number -> Number -> Number)
-opAddSub = opAdd <|> opSub
+-- AST
+data Op = Add | Sub
+data Expr = Factor Number | Infix Op Expr Expr
 
--- '*'
-opMul :: Parser (Number -> Number -> Number)
-opMul = char '*' `bind'` \_ -> return (*)
--- '/'
-opDiv :: Parser (Number -> Number -> Number)
-opDiv = char '/' `bind'` \_ -> return (/)
--- opM = '*' | '/'
-opMulDiv :: Parser (Number -> Number -> Number)
-opMulDiv = opMul <|> opDiv
+-- '+'
+opAdd :: Parser Op
+opAdd = char '+' `bind'` \_ -> return Add
+-- '-'
+opSub :: Parser Op
+opSub = char '-' `bind'` \_ -> return Sub
+-- opA = '+' | '-'
+opAddSub :: Parser Op
+opAddSub = opAdd <|> opSub
 
 -- digit = '0' | '1' | ... | '9'
 digit :: Parser Int
@@ -39,43 +34,26 @@ number = (many $ digit)
   where
     toInt = \acc x -> x + acc * 10
 
--- F = number | '(' expr ')'
-factor :: Parser Number
-factor = number
-
--- T = T opM F | F
--- T = F T'
-term :: Parser Number
-term = factor
-  `bind'` \f -> term'
-  `bind'` \op -> return $ op f
-
--- T' = opM F T' | e
-term' :: Parser (Number -> Number)
-term' = (opMulDiv
-  `bind'` \bop -> factor
-  `bind'` \f -> term'
-  `bind'` \op -> return (flip bop f >>> op))
-  <|> return identity
-
 -- E = T E'
 expr :: Parser Number
-expr = term
+expr = number
   `bind'` \t -> expr'
   `bind'` \op -> return $ op t
 
 -- E' = opA T E' | e
-expr' :: Parser (Number -> Number)
+expr' :: Parser ()
 expr' = (opAddSub
-  `bind'` \bop -> term
+  `bind'` \op -> number
   `bind'` \t -> expr'
-  `bind'` \op -> return (flip bop t >>> op))
+  `bind'` \e -> return $ Infix op t)
   <|> return identity
 
--- main :: String -> String
--- main value = """.intel_syntax noprefix
--- .global _main
--- _main:
---         mov rax, """ <> trim value <> """
---         ret
--- """
+main :: String -> String
+main value = """.intel_syntax noprefix
+.global _main
+_main:
+  mov rax, """ <> src <> """
+  ret
+"""
+  where
+    src = trim value
